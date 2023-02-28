fdf89f900f636e4bd215a2165aa36ac5709280e0
David <parrydavid@live.co.uk>
2023-02-28 17:05:04 +0000

    added in basic form

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -8,34 +8,70 @@ import appStyles from "../../App.module.css";
 import { Form, Button, Image, Col, Row, Container } from "react-bootstrap";
 
 const SignUpForm = () => {
-  return (
-    <Row className={styles.Row}>
-      <Col className="my-auto py-2 p-md-2" md={6}>
-        <Container className={`${appStyles.Content} p-4 `}>
-          <h1 className={styles.Header}>sign up</h1>
+    return (
+        <Row className={styles.Row}>
+            <Col className="my-auto py-2 p-md-2" md={6}>
+                <Container className={`${appStyles.Content} p-4 `}>
+                    <h1 className={styles.Header}>sign up</h1>
 
-            {/* add your form here */}
+                    <Form>
+                        <Form.Group controlId="Username">
+                            <Form.Label className="d-none" >Username</Form.Label>
+                            <Form.Control
+                                className={styles.Input}
+                                type="text"
+                                placeholder="Username"
+                                name="Username"
+                            />
+                        </Form.Group>
 
-        </Container>
-        <Container className={`mt-3 ${appStyles.Content}`}>
-          <Link className={styles.Link} to="/signin">
-            Already have an account? <span>Sign in</span>
-          </Link>
-        </Container>
-      </Col>
-      <Col
-        md={6}
-        className={`my-auto d-none d-md-block p-2 ${styles.SignUpCol}`}
-      >
-        <Image
-          className={`${appStyles.FillerImage}`}
-          src={
-            "https://codeinstitute.s3.amazonaws.com/AdvancedReact/hero2.jpg"
-          }
-        />
-      </Col>
-    </Row>
-  );
+                        <Form.Group controlId="password1">
+                            <Form.Label className="d-none">Password</Form.Label>
+                            <Form.Control
+                                className={styles.Input}
+                                type="password"
+                                placeholder="Password"
+                                name="password1"
+                            />
+                        </Form.Group>
+                        <Form.Group controlId="password2">
+                            <Form.Label className="d-none">Confirm Password</Form.Label>
+                            <Form.Control
+                                className={styles.Input}
+                                type="password"
+                                placeholder="Confirm Password"
+                                name="password2"
+                            />
+                        </Form.Group>
+
+                        <Button
+                            className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
+                            type="submit"
+                        >
+                            Sign Up
+                        </Button>
+                    </Form>
+
+                </Container>
+                <Container className={`mt-3 ${appStyles.Content}`}>
+                    <Link className={styles.Link} to="/signin">
+                        Already have an account? <span>Sign in</span>
+                    </Link>
+                </Container>
+            </Col>
+            <Col
+                md={6}
+                className={`my-auto d-none d-md-block p-2 ${styles.SignUpCol}`}
+            >
+                <Image
+                    className={`${appStyles.FillerImage}`}
+                    src={
+                        "https://codeinstitute.s3.amazonaws.com/AdvancedReact/hero2.jpg"
+                    }
+                />
+            </Col>
+        </Row>
+    );
 };
 
 export default SignUpForm;
