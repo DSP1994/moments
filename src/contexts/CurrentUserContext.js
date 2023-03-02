a4c04fbe0ea8d66a063bfe7a8a31d88dcd26790b
David <parrydavid@live.co.uk>
2023-03-02 11:18:14 +0000

    finalised current user context file

--- src/contexts/CurrentUserContext.js
+++ src/contexts/CurrentUserContext.js
@@ -1,1 +1,33 @@
-export const CurrentUserProvider = ({ children }) => { }
+import { createContext, useEffect, useState } from "react";
+import axios from "axios";
+
+export const CurrentUserContext = createContext()
+export const setCurrentUserContext = createContext()
+
+export const useCurrentUser = () => useContext(CurrentUserContext)
+export const useSetCurrentUser = () => CurrentUserContext(setCurrentUserContext)
+
+export const CurrentUserProvider = ({ children }) => {
+    const [currentUser, setCurrentUser] = useState(null)
+
+    const handleMount = async () => {
+        try {
+            const { data } = await axios.get('dj-rest-auth/user/')
+            setCurrentUser(data)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    useEffect(() => {
+        handleMount()
+    }, []);
+
+    return (
+        <CurrentUserContext.Provider value={currentUser}>
+            <setCurrentUserContext.Provider value={setCurrentUser}>
+                {children}
+            </setCurrentUserContext.Provider>
+        </CurrentUserContext.Provider>
+    )
+}
